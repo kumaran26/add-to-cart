dcac4f0a3e8efffb951d218868f2d92bfa54b157
kumaran <kumaran26skr@gmail.com>
2021-06-19 22:35:31 +0200

    CSS: Updated the styles as per the mockup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
   function displayItems(){
     var items = '';
     data.forEach(function(item){
-      items = items + '<div class="item"><img class="cart-display-image" src=' + item.image + '><div class="cart-offer">' + item.discount + '% Off</div><div class="item-info"><div class="cart-item-wrapper"><div class="mar-l10">' + item.name  + '</div></div><div><span class="mar-l10 cart-summary-strike">$'+ item.price.display +'</span><span class="mar-l10 cart-summary-display">$'+ item.price.actual + '</span></div><div class="cart-add-wrapper"><div class="cart-add" onclick="addToCart(\'' + item.name + '\')">Add to cart</div></div></div></div>';
+      items = items + '<div class="item"><img class="cart-display-image" src=' + item.image + '><div class="cart-offer">' + item.discount + '% Off</div><div class="item-info"><div class="cart-item-wrapper"><div class="mar-l10">' + item.name  + '</div></div><div><span class="mar-l10 cart-summary-strike-color"><span class="cart-summary-strike">$'+ item.price.display +'</span></span><span class="mar-l10 cart-summary-display">$'+ item.price.actual + '</span></div><div class="cart-add-wrapper"><div class="cart-add" onclick="addToCart(\'' + item.name + '\')">Add to cart</div></div></div></div>';
     });
     document.getElementById('items').innerHTML = items;
   }
