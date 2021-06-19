8ddca977af56db6139168f11fe4287f46b9783df
kumaran <kumaran26skr@gmail.com>
2021-06-19 19:23:02 +0200

    feat: Show alert on clicking add to cart button

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
   
-  var data = []
+  var data = [];
   
   function displayItems(){
-	var items = '';
-	data.forEach(function(item){
-		items = items + '<div class="item"><img class="cart-display-image" src=' + item.image + '><div class="cart-offer">' + item.discount + '% Off</div><div class="item-info"><div class="cart-item-wrapper"><div class="mar-l10">' + item.name  + '</div></div><div><span class="mar-l10 cart-summary-strike">$'+ item.price.display +'</span><span class="mar-l10 cart-summary-display">$'+ item.price.actual + '</span></div><div class="cart-add-wrapper"><div class="cart-add" onclick="addToCart(\'' + item.name + '\')">Add to cart</div></div></div></div>';
-	});
-	document.getElementById('items').innerHTML = items;
+    var items = '';
+    data.forEach(function(item){
+      items = items + '<div class="item"><img class="cart-display-image" src=' + item.image + '><div class="cart-offer">' + item.discount + '% Off</div><div class="item-info"><div class="cart-item-wrapper"><div class="mar-l10">' + item.name  + '</div></div><div><span class="mar-l10 cart-summary-strike">$'+ item.price.display +'</span><span class="mar-l10 cart-summary-display">$'+ item.price.actual + '</span></div><div class="cart-add-wrapper"><div class="cart-add" onclick="addToCart(\'' + item.name + '\')">Add to cart</div></div></div></div>';
+    });
+    document.getElementById('items').innerHTML = items;
   }
   
   fetch('data/items.json')
@@ -20,5 +20,18 @@
   });
   
   function addToCart(item){
-	
+    document.getElementsByClassName('cart-header-alert')[0].style.display = 'block';
+    document.getElementsByClassName('cart-header-alert')[0].innerHTML = item + ' is added to the cart';
+    setTimeout(function(){
+      document.getElementsByClassName('cart-header-alert')[0].style.display = 'none';
+    }, 1000);
   }
+
+  fetch('data/items.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    data = jsonResponse ? jsonResponse.items : [];
+	  displayItems();
+  });
